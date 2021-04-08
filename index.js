a39dee1e67ef498c6daf54c0e14056feef929b5b
Samuel <samuel@brandani.net.br>
2021-04-08 16:27:45 -0300

    final

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.send('rodando')
 })
-router.get('/characters', service.getAll)
-router.get('/characters/:id', service.getById)
-router.get('/characters/:id/comics', service.getByComics)
-router.get('/characters/:id/events', service.getByEvent)
-router.get('/characters/:id/series', service.getBySerie)
-router.get('/characters/:id/stories', service.getByStory)
+router.get('/v1/public/characters', service.getAll)
+router.get('/v1/public/characters/:id', service.getById)
+router.get('/v1/public/characters/:id/comics', service.getByComics)
+router.get('/v1/public/characters/:id/events', service.getByEvent)
+router.get('/v1/public/characters/:id/series', service.getBySerie)
+router.get('/v1/public/characters/:id/stories', service.getByStory)
 
 
 app.use('/', router)
