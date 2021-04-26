31fa232770c211602094e366d4fcaafc2e9e5d7a
gabriel.ribeiro <gabriel.ribeiro@ifsuldeminas.edu.br>
2021-04-26 14:23:39 -0300

    criacao do docker e documentacao no swagger

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # marvel-api
+# Link do docker : https://hub.docker.com/repository/docker/gabriel1233/marvelapi
+# Link documentation swagger : https://app.swaggerhub.com/apis/GabrielRicieri/Api_Marvel/1.0.0#/Manufacturer
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const service = require('./src/services')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('./documentation.json');
 
 const router = express.Router()
 router.get('/', (req, res) => {
@@ -13,8 +15,93 @@ router.get('/v1/public/characters/:id/events', service.getByEvent)
 router.get('/v1/public/characters/:id/series', service.getBySerie)
 router.get('/v1/public/characters/:id/stories', service.getByStory)
 
-
+app.use(express.json());
 app.use('/', router)
+
+//Documentação server SwaggerHub: https://app.swaggerhub.com/apis/BlazeF35/API_marvel/1.0.0
+//const swaggerOptions = {
+ //   swaggerDefinition: {
+ //       info: {
+  //          title: 'MarvelAPI',
+   //         description: 'API Marvel',
+   //         version: '1.0.0',
+   //         contact: {
+    //            name: 'Samuel Alves Brandani e Gabriel Ricieri Martines Ribeiro',
+  //              description: '',
+///email: ''
+ //           },
+ //           servers: ['http://localhost:8081']
+ //       }
+  //  },
+  //  apis: ['index']
+//};
+
+//const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc));
+// Routes
+/**
+ * /Characters:
+ *  get:
+ *      summary: API disponibilizada pela Marvel
+ *      description: Lista todos os nomes dos persoangens, revistas em quadrinho e sua data de lançamento
+ *      consumes: 
+ *      - application/json
+ *      produces:
+ *      - applicatio/json
+ *      parameters:
+ *      - in: body
+ *        schema:
+ *        $ref: '#/definition/Characters'
+ *      responses:
+ *        201: 
+ *          description: Requisição Realizada
+ *        400:
+ *          description: Erro de Requisição
+ *        500:
+ *          description: Erro interno servidor
+ *      
+ *        definition:
+ *          Characters:
+ *              type: string
+ *               required:
+ *                  - comics
+ *                  - events
+ *                  - series
+ *                  - stories
+ *          properties:
+ *              id:
+ *                  type: string
+ *                  format: uuid
+ *                  example: 1011334
+ *              comics:
+ *                  type: string
+ *                  example: Iron Man
+ *              events:
+ *                  type: string
+ *                  format: date
+ *                  example: 1963-03-03
+ *              series:
+ *                  type: string
+ *                  example: Avengers Assemble
+ *              stories:
+ *                  type: string 
+ *                  example: Invincible Iron Man
+ *              manufacturer:
+ *                  $ref: '#/definitions/Manufacturer'
+ *         Manufacturer: 
+ *              required:
+ *              - Marvel ApiKey
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                      example: Marvel
+ *                  homePage:
+ *                      type: string
+ *                      format: url
+ *                      example: https://developer.marvel.com/
+ * 
+ * host: http://localhost:8081
+ */
 
 const porta = process.env.PORT;
 console.log(porta)
