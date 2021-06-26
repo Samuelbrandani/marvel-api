0f3a498e38e340ca8bf96be4d0e762a52b6aa44a
GabrielRicieri <gabrielmartines145@gmail.com>
2021-06-26 11:10:24 -0300

    adiçao de test e workflow

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const porta = process.env.PORT;
 console.log(porta)
 
 app.listen(porta || 8081, () => {
-    console.log('Server started on port 8081')
+    console.log('Server starteds on port 8081')
 })
